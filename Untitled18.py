55d7e64199bfaf389f1afd91bdba0066b86fd61d
sumitprakashdubey <sumitprakashdubey@gmail.com>
2019-07-12 23:38:20 +0530

    Add files via upload

---
+++ Untitled18.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Swap Values without third or temp Variable
+
+# In[16]:
+
+
+a,b=100,200
+
+
+# In[17]:
+
+
+print('The value before swapping is {} and {}'.format(a,b))
+
+
+# In[18]:
+
+
+a,b=b,a
+
+
+# In[19]:
+
+
+print('The value after sawapping is {} and {}'.format(a,b))
+
+
+# In[ ]:
+
+
+
+
